18b39f615442748a635cb03a423038bd579d7a40
otooto <hibikun1998@icloud.com>
2021-08-27 00:31:02 +0900

    modal add type_jQuery

--- modal/modal.js
+++ modal/modal.js
@@ -1,34 +1,76 @@
+$(function(){
+    console.log('read jQuery File!');
+});
+
+$(function(){
+    $('h1').css('color', '#00eeff');
+});
 //console.log(document.querySelector(".js-modal-open"));
 
 //querySelectorでセレクターで指定したidを持つ
-document.querySelector('.js-modal-open').addEventListener('click',test1());
+document.querySelector('.js_modal_open').addEventListener('click',test1());
 
-const open = document.getElementById('open');
+//modal_content下の要素を取得する。
+const modal = document.getElementById('modal_content');
 
-const modal = document.getElementById('modal');
 
-console.log(document.getElementById('modal'));
+$(function(){
+    $('.js_modal_open').on('click',function(){
+        console.log('push');
+        $('.modal_fadeIn').fadeIn();
+        return false;
+    });
+});
+
+let close_modal =() => {
+
+    $('.modal_fadeIn').fadeOut();
+    return false;
+
+};
+
+/*
+    $('.js-modal-close').on('click',function(){
+        $('.js-modal').fadeOut();
+        return false;
+    });
+    */
+
+
+
+//クリックしたら、hiddenを消すことで隠れている内容を表示する。
+function openmodalwindow(){
+
+    $('modal_content').fadeIn();
+
+    return false;
+
+    modal.classList.remove('hidden');
+
+}
+
+//逆にhiddenを追加して、モーデルを隠す
+/*
+function close_modal(){
+
+    modal.classList.add('hidden');
+
+}
+*/
+
+//モーダル表示中に画面をクリックすると、その時だけ動いている。
+modal.addEventListener('click',() =>{
+
+    close_modal();
+    test1();
+});
+
+
+console.log(document.getElementById('modal_content'));
 
 function test1(){
 
     console.log("test");
 }
 
-function openmodalwindow(){
-
-    modal.classList.remove('hidden');
-
-}
-
-function close_modal(){
-
-    modal.classList.add('hidden');
-
-
-}
-
-
-function closeModalWindow(modalElement) {
-    document.body.removeChild(modalElement);
-}
 
